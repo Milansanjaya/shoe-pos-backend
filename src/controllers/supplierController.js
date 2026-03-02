67185d54d7e90003ca2138040c892ea8208b9f7b
Milan sanjaya <sanjaya2170@gmail.com>
2026-03-02 14:43:02 +0530

    update

--- src/controllers/supplierController.js
+++ src/controllers/supplierController.js
@@ -1,4 +1,6 @@
 const Supplier = require("../models/Supplier");
+const Purchase = require("../models/Purchase");
+const Product = require("../models/Product");
 
 // Create supplier
 exports.createSupplier = async (req, res) => {
@@ -15,6 +17,37 @@ exports.getSuppliers = async (req, res) => {
   try {
     const suppliers = await Supplier.find();
     res.json(suppliers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Delete supplier
+exports.deleteSupplier = async (req, res) => {
+  try {
+    const supplierId = req.params.id;
+
+    const hasPurchases = await Purchase.exists({ supplier: supplierId });
+    if (hasPurchases) {
+      return res.status(400).json({
+        message: "Cannot delete supplier because purchases exist for this supplier"
+      });
+    }
+
+    const hasProducts = await Product.exists({ supplier: supplierId });
+    if (hasProducts) {
+      return res.status(400).json({
+        message: "Cannot delete supplier because products exist for this supplier"
+      });
+    }
+
+    const supplier = await Supplier.findByIdAndDelete(supplierId);
+
+    if (!supplier) {
+      return res.status(404).json({ message: "Supplier not found" });
+    }
+
+    res.json({ message: "Supplier deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
